f3e77bd05783d887ccc25a4610e4ce3ea8a8de54
Mingding <hanmd82@gmail.com>
2017-01-02 22:01:27 +0800

    Draw basic pie chart from residential data

--- app/assets/javascripts/residential.js
+++ app/assets/javascripts/residential.js
@@ -1,2 +1,40 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  var width = 960,
+     height = 500,
+     radius = Math.min(width, height) / 2;
+
+  var totals = {};
+  var color  = d3.scale.category20c();
+
+  var arc = d3.svg.arc()
+                  .outerRadius(radius - 10)
+                  .innerRadius(0);
+
+  var pie = d3.layout.pie()
+                     .sort(null)
+                     .value(function(d) { return totals[d]; });
+
+  var svg = d3.select("body").append("svg")
+                              .attr("width", width)
+                              .attr("height", height)
+                             .append("g")
+                              .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  $.getJSON('/residential/data', function(data) {
+    totals = data.totals;
+
+    var g = svg.selectAll(".arc")
+               .data(pie(d3.keys(totals)))
+               .enter().append("g")
+                         .attr("class", "arc");
+
+        g.append("path")
+          .attr("d", arc)
+          .style("fill", function(d) { return color(d.data); });
+        g.append("text")
+          .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+          .attr("dy", ".35em")
+          .style("text-anchor", "middle")
+          .text(function(d) { return d.data; });
+  });
+});
