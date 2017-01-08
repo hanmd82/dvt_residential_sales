11fea00fcc2430bcb866430aac36762ac1efeef8
Mingding <hanmd82@gmail.com>
2017-01-08 18:05:46 +0800

    Add bar chart to show total sales by zipcode

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'residential/index'
   get 'residential/data', defaults: { format: 'json' }
 
+  get 'residential/bar_chart'
+  get 'residential/bar_data', defaults: { format: 'json' }
+
   root 'residential#index'
 end
